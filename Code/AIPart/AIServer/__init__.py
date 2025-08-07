3ab497a48e2ab058de848c2b1aff3f2b85bed100
ricckker <3356203629@qq.com>
2025-08-07 22:25:36 +0800

    完成WebSocket服务端搭建

---
+++ Code/AIPart/AIServer/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+包名称: AIServer
+简介: 基于WebSocket的服务端，支持热修改、调试模式
+版本: 1.0.0
+作者: Yingrui Chen / QG Studio
+许可证: MIT
+主页: https://github.com/LS-XH/Park_Intelligent_Platform
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__all__ = [
+    'WebSocketServer'
+]
+
+# 包的元数据
+__version__ = "1.0.0"
+__author__ = "Yingrui Chen / QG Studio"
+__license__ = "MIT"
+__description__ = "A Websocket server for AI Designed by Yingrui Chen"
+__url__ = "https://github.com/LS-XH/Park_Intelligent_Platform"
