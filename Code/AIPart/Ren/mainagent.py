47294c8f719f581bf306aafb7fea8c421edf3162
LZX <3267343261@qq.com>
2025-08-13 22:50:38 +0800

    0813

--- Code/AIPart/Ren/mainagent.py
+++ Code/AIPart/Ren/mainagent.py
@@ -2,7 +2,10 @@ import numpy as np
 import torch
 
 from Code.AIPart.Interface.people import PersonBase
-from model import ObstacleGenerator, AgentGroup
+from Code.AIPart.Ren.csxiaofeng import crowd_evacuation
+from Code.AIPart.Ren.agents import AgentGroup
+from Code.AIPart.Ren.obstacles import ObstacleGenerator
+from Code.AIPart.Ren.config import *
 
 map_path = "testmap.py"
 
@@ -11,34 +14,38 @@ map_path = "testmap.py"
 class Crowd(PersonBase):
 
     @staticmethod
-    def create_agents(num_agents, map_matrix, intersection_id_map, obstacle_gen):
-        return AgentGroup(num_agents, map_matrix, intersection_id_map, obstacle_gen)
+    def create_agents(num_agents, map_matrix, intersection_id_map, obstacle_gen, targets, targets_heat):
+        return AgentGroup(num_agents, map_matrix, intersection_id_map, obstacle_gen, targets, targets_heat)
 
-    @staticmethod
-    def get_emergency(happened: list = None):
-        if happened:
-            for happened in happened:
-                e=666
-        return []
+    def get_emergency(self, happens: list = None):
+        emergency = []
+        if happens:
+            dm = self.agents.density.T
+            for happen in happens:
+                (x, y), desc = happen
+                x, y = x * DENSITY_MATRIX_SIZE / MAP_SIZE, y * DENSITY_MATRIX_SIZE / MAP_SIZE
+                dens = dm[x][y]
+                level = crowd_evacuation(dens, desc)
+        return emergency
 
-    @staticmethod
-    def deal_trafficlight(trafficlight: np.array = None)->list:
-        """
-
-        :param trafficlight:
-        :return:
-        """
-        light = []
-        try:
-            for i in range(len(trafficlight[0])):
-                if max(trafficlight[:,i]) < 1:
-                    light.append(1)
-                else:
-                    light.append(0)
-            return light
-        except IndexError:
-            print("Traffic Light Matrix Is Empty")
-            return []
+    # @staticmethod
+    # def deal_trafficlight(trafficlight: np.array = None)->list:
+    #     """
+    #
+    #     :param trafficlight:original trafficlight matrix
+    #     :return:
+    #     """
+    #     light = []
+    #     try:
+    #         for i in range(len(trafficlight[0])):
+    #             if max(trafficlight[:,i]) < 1:
+    #                 light.append(1)
+    #             else:
+    #                 light.append(0)
+    #         return light
+    #     except IndexError:
+    #         print("Traffic Light Matrix Is Empty")
+    #         return []
 
     @staticmethod
     def make_obs():
@@ -47,8 +54,9 @@ class Crowd(PersonBase):
         obs = ObstacleGenerator(map_size)
         obs.add_obstacles(obs_params)
         return obs
+
     @staticmethod
-    def deal_map(graph:any) -> (list, np.array, list):
+    def deal_map(graph:any) -> (list, np.ndarray, list):
         """
 
         :return: obstacles_params, targets, points
@@ -72,11 +80,11 @@ class Crowd(PersonBase):
     def simulate(self, happened: list = None, trafficlight: list = None):
         """
 
-        :param happened: [((x,y), str), ...]
-        :param trafficlight:
-        :return:
+        :param happened: [((x,y), str), ...]  (pos, describe)
+        :param trafficlight: [1, 0, 1, 0, ...]  id - statu
+        :return: no return
         """
-        emergency = self.get_emergency(happened=happened)
+        emergency = self.get_emergency(happened)
         eme_pos = []
         eme_lev = []
         if emergency:
@@ -84,25 +92,48 @@ class Crowd(PersonBase):
                 eme_pos.append(pos)
                 eme_lev.append(lev)
         self.agents.move_towards_target((eme_pos, eme_lev), trafficlight)
-        pass
 
-
-    def get_pos(self, node_id: int, ranges: int = 30) -> list:
-
-        pass
+    # def get_pos(self, node_id: int, radius: int = 30) -> list:
+    #     pass
 
     @property
-    def density(self) -> float:
-        return self.agents.get_density().T
+    def density(self) -> np.ndarray:
+        """
+        :return: density (DENSITY_MATRIX_SIZE * DENSITY_MATRIX_SIZE)
+        """
+        return self.agents.get_density().cpu().numpy().T
 
     @property
     def position(self) -> list:
-        positions = self.agents.positions.cpu().numpy()
-        return []
+        """
+        :return: [(x, y), ...]
+        """
+        positions = self.agents.positions.cpu().numpy().tolist()
+        return positions
+
+    def kill(self, node_id: int, radius: int = 20, num: int = 30):
+        """
+        be same to name
+        :param node_id:
+        :param radius:
+        :param num:
+        :return:
+        """
+        x, y = points[node_id]
+        self.agents.kill(x, y, radius, num)
 
 
-    def kill(self, node_id: int, ranges: int = 20):
-        pass
+    def birth(self, node_id: int, radius: int = 20, num: int = 30):
+        """
+        be same to name
+        :param node_id:
+        :param radius:
+        :param num:
+        :return:
+        """
+        x, y = points[node_id]
+        self.agents.birth(x, y, radius, num)
+
 
 
 
